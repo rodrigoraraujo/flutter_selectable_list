9d26100efad5924f23e61b4375ca346decf7e783
Rodrigo Ramalho Araujo <rodrigoraraujo@gmail.com>
2020-12-13 00:19:53 -0300

    Second Version

--- lib/main.dart
+++ lib/main.dart
@@ -6,120 +6,166 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: SafeArea(
-          child: SelectableListView(),
-        ),
-      ),
-    );
-  }
-}
-
-enum ScreenMode { edit, browse }
-
-class SelectableListView extends StatefulWidget {
-  @override
-  _SelectableListViewState createState() => _SelectableListViewState();
-}
-
-class _SelectableListViewState extends State<SelectableListView> {
-  final listSelectableItems = List.generate(
-    20,
-    (index) => SelectableListViewItem(
-      title: 'Item $index',
-    ),
-  );
-
-  List<Widget> listTileItems;
-  List<Widget> currentList;
-  ScreenMode mode = ScreenMode.browse;
-
-  @override
-  void initState() {
-    super.initState();
-
-    listTileItems = List.generate(
-      20,
-      (index) => ListTile(
-        title: Text('Item $index'),
-        onLongPress: () {
-          setState(() {
-            currentList = List.generate(
-              20,
-              (i) => SelectableListViewItem(
-                title: 'Item $i',
-                selected: index == i,
-              ),
-            );
-            mode = ScreenMode.edit;
-          });
-        },
-      ),
-    );
-
-    currentList = listTileItems;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Visibility(
-          visible: mode == ScreenMode.edit,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                icon: Icon(
-                  Icons.close,
-                ),
-                onPressed: () {
-                  setState(() {
-                    currentList = listTileItems;
-                    mode = ScreenMode.browse;
-                  });
-                },
-              ),
-              FlatButton.icon(
-                icon: Icon(Icons.delete),
-                label: Text('Delete all'),
-                onPressed: () {},
-              ),
-            ],
+      theme: ThemeData.light().copyWith(
+          //primarySwatch: Colors.white.,
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-              itemCount: listSelectableItems.length,
-              itemBuilder: (context, index) => currentList[index]),
-        ),
-      ],
+      home: CustomInheritedWidget(
+        child: CustomWidget(),
+      ),
     );
   }
 }
 
-class SelectableListViewItem extends StatefulWidget {
+enum ScreenMode { browse, edit }
+
+class Model {
   final String title;
   final bool selected;
 
-  SelectableListViewItem({this.title, this.selected = false});
-
-  @override
-  _SelectableListViewItemState createState() => _SelectableListViewItemState();
+  Model({this.title, this.selected});
 }
 
-class _SelectableListViewItemState extends State<SelectableListViewItem> {
+class CustomInheritedWidget extends InheritedWidget {
+  CustomInheritedWidget({Widget child}) : super(child: child);
+
+  final List<Model> selectedItems = [];
+  final ValueNotifier<int> itemsLength = ValueNotifier(0);
+  final ValueNotifier<ScreenMode> screenMode = ValueNotifier(ScreenMode.browse);
+
+  static CustomInheritedWidget of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CustomInheritedWidget>();
+  }
+
+  @override
+  bool updateShouldNotify(CustomInheritedWidget oldWidget) =>
+      oldWidget.selectedItems.length != selectedItems.length;
+}
+
+class CustomWidget extends StatefulWidget {
+  @override
+  _CustomWidgetState createState() => _CustomWidgetState();
+}
+
+class _CustomWidgetState extends State<CustomWidget> {
+  static final data = List.generate(
+    5,
+    (index) => Model(
+      title: 'Item ${index + 1}',
+      selected: false,
+    ),
+  );
+
+  bool allItemsSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: CustomInheritedWidget.of(context).itemsLength,
+        builder: (context, _, __) {
+          allItemsSelected =
+              CustomInheritedWidget.of(context).selectedItems.length ==
+                  data.length;
+
+          return ValueListenableBuilder(
+            valueListenable: CustomInheritedWidget.of(context).screenMode,
+            builder: (context, _, __) => Scaffold(
+              appBar: CustomInheritedWidget.of(context).screenMode.value ==
+                      ScreenMode.edit
+                  ? AppBar(
+                      leading: IconButton(
+                        icon: Icon(Icons.close),
+                        onPressed: () {
+                          CustomInheritedWidget.of(context)
+                              .selectedItems
+                              .clear();
+                          CustomInheritedWidget.of(context).screenMode.value =
+                              ScreenMode.browse;
+                        },
+                      ),
+                      title: Text(
+                        '${CustomInheritedWidget.of(context).selectedItems.length} selected',
+                      ),
+                      actions: [
+                        IconButton(
+                          icon: Icon(allItemsSelected
+                              ? Icons.check_box_outline_blank
+                              : Icons.check),
+                          onPressed: () {
+                            setState(() {
+                              CustomInheritedWidget.of(context)
+                                  .selectedItems
+                                  .clear();
+                              if (!allItemsSelected) {
+                                for (final model in data) {
+                                  CustomInheritedWidget.of(context)
+                                      .selectedItems
+                                      .add(
+                                        Model(
+                                          title: model.title,
+                                          selected: true,
+                                        ),
+                                      );
+                                }
+                              }
+                            });
+                          },
+                        ),
+                        Visibility(
+                          visible: CustomInheritedWidget.of(context)
+                                  .selectedItems
+                                  .length >
+                              0,
+                          child: IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    )
+                  : AppBar(
+                      title: Text('List'),
+                    ),
+              body: SafeArea(
+                child: ListView.builder(
+                  key: UniqueKey(),
+                  itemCount: data.length,
+                  itemBuilder: (_, index) => CustomLIstItem(
+                    title: data[index].title,
+                    selected: CustomInheritedWidget.of(context)
+                        .selectedItems
+                        .firstWhere(
+                          (element) => element.title == data[index].title,
+                          orElse: () => Model(title: '', selected: false),
+                        )
+                        .selected,
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class CustomLIstItem extends StatefulWidget {
+  final String title;
+  final bool selected;
+
+  CustomLIstItem({
+    @required this.title,
+    @required this.selected,
+  });
+
+  @override
+  _CustomLIstItemState createState() => _CustomLIstItemState();
+}
+
+class _CustomLIstItemState extends State<CustomLIstItem> {
   bool _selected;
-  final selectedColor = Colors.blue;
-  final unselectedColor = Colors.grey;
 
   @override
   void initState() {
@@ -129,25 +175,42 @@ class _SelectableListViewItemState extends State<SelectableListViewItem> {
 
   @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(
-      controlAffinity: ListTileControlAffinity.leading,
-      selected: _selected,
-      title: Text(widget.title),
-      value: _selected,
-      onChanged: (value) {
-        setState(() {
-          _selected = value;
-        });
-      },
-      // onTap: (){
-      //  setState(() {
-      //    _selected = false;
-      //  });
-      // },
-      // onLongPress: () {
-      //   setState(() {
-      //     _selected = !_selected;
-      //   });
-    );
+    return ListTile(
+        leading:
+            _selected ? Icon(CupertinoIcons.check_mark_circled_solid) : null,
+        selected: _selected,
+        title: Text(widget.title),
+        onLongPress: CustomInheritedWidget.of(context).screenMode.value ==
+                ScreenMode.edit
+            ? () {}
+            : () {
+                CustomInheritedWidget.of(context).screenMode.value =
+                    ScreenMode.edit;
+                final model = Model(title: widget.title, selected: true);
+                CustomInheritedWidget.of(context).selectedItems.add(model);
+              },
+        onTap: CustomInheritedWidget.of(context).screenMode.value ==
+                ScreenMode.edit
+            ? () {
+                setState(() {
+                  _selected = !_selected;
+                });
+
+                final model = Model(title: widget.title, selected: _selected);
+
+                if (_selected) {
+                  CustomInheritedWidget.of(context).selectedItems.add(model);
+                } else {
+                  CustomInheritedWidget.of(context).selectedItems.removeWhere(
+                        (element) => element.title == widget.title,
+                      );
+                }
+
+                CustomInheritedWidget.of(context).itemsLength.value =
+                    CustomInheritedWidget.of(context).selectedItems.length;
+
+                print(CustomInheritedWidget.of(context).selectedItems.length);
+              }
+            : () {});
   }
 }
--- lib/selectable_listview.dart
+++ lib/selectable_listview.dart
@@ -1,10 +1,52 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SelectableListView extends StatelessWidget {
+  final List<String> data;
+  final int selectedIndex;
+
+  SelectableListView({
+    @required this.data,
+    @required this.selectedIndex,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(),
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        actions: [
+          FlatButton.icon(
+            icon: Icon(Icons.check_box),
+            label: Text('Select all'),
+            onPressed: () {},
+          ),
+          FlatButton.icon(
+            icon: Icon(CupertinoIcons.delete),
+            label: Text('Delete all'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+
+        children: data
+            .map(
+              (item) => SelectableListViewItem(
+                title: item,
+                selected: item == data[selectedIndex],
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
@@ -13,7 +55,10 @@ class SelectableListViewItem extends StatefulWidget {
   final String title;
   final bool selected;
 
-  SelectableListViewItem({this.title, this.selected = false});
+  SelectableListViewItem({
+    @required this.title,
+    this.selected = false,
+  });
 
   @override
   _SelectableListViewItemState createState() => _SelectableListViewItemState();
